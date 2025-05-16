bb9e5cd6cfcb7d7de1adf76b175b62857a0e400a
Fenzakosu <lukakovacevic266@gmail.com>
2025-05-16 12:04:58 +0200

    feat:modeli i repozitorijumi za korisnike i grupe , kontroler za korisnika , dobavljanje svih korisnika za grupu preko kontrolera

---
+++ 0601DrustvenaMreza/Controller/GrupaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _0601DrustvenaMreza.Controller
+{
+    [Route("api/grupe")]
+    [ApiController]
+    public class GrupaController : ControllerBase
+    {
+
+    }
+}
---
+++ 0601DrustvenaMreza/Controller/GrupaKorisnikController.cs
@@ -0,0 +1,36 @@
+﻿using _0601DrustvenaMreza.Model;
+using _0601DrustvenaMreza.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _0601DrustvenaMreza.Controller
+{
+    [Route("api/grupe/{grupaId}/korisnici")]
+    [ApiController]
+    public class GrupaKorisnikController : ControllerBase
+    {
+        private GrupaRepo grupaRepo = new GrupaRepo();
+        private KorisnikRepo korisnikRepo = new KorisnikRepo();
+
+        [HttpGet]
+        public ActionResult<List<Korisnik>> Get(int grupaId)
+        {
+            if (!GrupaRepo.Data.ContainsKey(grupaId))
+            {
+                return NotFound();
+            }
+
+            List<Korisnik> sviKorisnici = KorisnikRepo.Data.Values.ToList();
+            List<Korisnik> korisniciGrupe = new List<Korisnik>();
+            foreach (Korisnik korisnik in sviKorisnici)
+            {
+                if (korisnik.grupe.Count != 0 && korisnik.grupe.ContainsKey(grupaId))
+                {
+                    korisniciGrupe.Add(korisnik);
+                }
+            }
+
+            return Ok(korisniciGrupe);
+        }
+    }
+}
